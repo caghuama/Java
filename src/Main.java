3b7f13946c430c8efd2bb2ba6c47c14bafd35ad5
Cesar <cesar.arg92@gmail.com>
2019-06-03 19:16:32 -0500

    Pequeña info sobre consultor

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,5 +9,24 @@ public class Main {
 
         System.out.println("Prueba de git");
         System.out.println("Segunda prueba para github");
+
+
+        //Pruebas para repo de github
+        String nombres = "Cesar Alonso";
+        String apellido_pat ="Romero";
+        String apellido_mat ="Garcia";
+        int experiencia = 2;
+        String ocupacion = "Consultor";
+        String empresa = "MopegaTI";
+        int ID = 31241;
+
+
+        JOptionPane.showMessageDialog(null,
+                "Buenas tardes Sr. "+ nombres +" "+ apellido_pat +" "+  apellido_mat + "\n" + "," +
+                "la base de datos muestra que usted ocupa el puesto de: " + ocupacion + " en la Empresa: "+ empresa + "\n" +
+                " con el Id de empleado: "+ ID +  ", actualmente tiene "+ experiencia + " años de experiencia.");
+
+
+
     }
 }
